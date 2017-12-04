071ba6a270c053a47c5118724e23febf6d60c735
BetterZT <794414136@qq.com>
2017-12-04 22:55:22 +0800

    第一次提交

---
+++ Project2.0_20170808193525/output/DefineID.h
@@ -0,0 +1,406 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+//创建画面对话框1的命令索引ID
+#define  _SCREEN_对话框1                                                          1
+
+//创建画面保存出错的命令索引ID
+#define  _SCREEN_保存出错                                                          2
+
+//创建画面完成的命令索引ID
+#define  _SCREEN_完成                                                            3
+
+//创建画面系统的命令索引ID
+#define  _SCREEN_系统                                                            4
+
+//创建画面生产的命令索引ID
+#define  _SCREEN_生产                                                            5
+
+//创建画面本产品的命令索引ID
+#define  _SCREEN_本产品                                                           6
+
+//创建画面生产厂家的命令索引ID
+#define  _SCREEN_生产厂家                                                          7
+
+//创建画面功能与作用的命令索引ID
+#define  _SCREEN_功能与作用                                                         8
+
+//创建画面测试参数的命令索引ID
+#define  _SCREEN_测试参数                                                          9
+
+//创建画面仪器操作的命令索引ID
+#define  _SCREEN_仪器操作                                                         10
+
+//创建画面应用领域的命令索引ID
+#define  _SCREEN_应用领域                                                         11
+
+//创建画面应用案例的命令索引ID
+#define  _SCREEN_应用案例                                                         12
+
+//创建画面测试参数2的命令索引ID
+#define  _SCREEN_测试参数2                                                        13
+
+//创建画面显示的命令索引ID
+#define  _SCREEN_显示                                                           14
+
+//画面Screen0的背景图片
+#define  _IMG_SCREEN0                                                          0
+
+#define  _PROGRESS_SCREEN0_PROGRESS1                                          24
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY3                                       6
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY4                                       7
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY14                                     17
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY17                                     20
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY15                                     18
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY16                                     19
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY18                                     21
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY6                                       9
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY7                                      10
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY8                                      11
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY9                                      12
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY10                                     13
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY11                                     14
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY12                                     15
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY13                                     16
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY21                                     34
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY22                                     35
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY19                                     31
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY20                                     32
+
+#define  _GRAPH_SCREEN0_GRAPH1                                                23
+
+#define  _GRAPH_SCREEN0_GRAPH2                                                33
+
+#define  _BTN_SCREEN0_BUTTON1                                                  3
+
+#define  _BTN_SCREEN0_BUTTON2                                                  4
+
+#define  _BTN_SCREEN0_BUTTON4                                                 27
+
+#define  _BTN_SCREEN0_BUTTON3                                                 28
+
+#define  _BTN_SCREEN0_BUTTON5                                                  1
+
+#define  _BTN_SCREEN0_BUTTON6                                                  2
+
+#define  _BTN_SCREEN0_BUTTON7                                                  5
+
+#define  _BTN_SCREEN0_BUTTON8                                                 22
+
+//画面Screen0中动画控件Animation1使用的图片
+#define  _IMG_SCREEN0_ANIMATION1                                               1
+
+#define  _ANIMATION_SCREEN0_ANIMATION1                                        26
+
+//画面Screen0中动画控件Animation2使用的图片
+#define  _IMG_SCREEN0_ANIMATION2                                               2
+
+#define  _ANIMATION_SCREEN0_ANIMATION2                                        29
+
+#define  _TXT_DIS__SCREEN0_CLC                                                 8
+
+//画面对话框1中图片Image1使用的图片
+#define  _IMG_对话框1_IMAGE1                                                      3
+
+#define  _TXT_DIS__对话框1_TEXT_DISPLAY1                                          2
+
+#define  _BTN_对话框1_BUTTON1                                                     4
+
+#define  _BTN_对话框1_BUTTON2                                                     3
+
+//画面保存出错中图片Image1使用的图片
+#define  _IMG_保存出错_IMAGE1                                                      4
+
+#define  _GRAPH_保存出错_GRAPH1                                                    2
+
+//画面完成中图片Image1使用的图片
+#define  _IMG_完成_IMAGE1                                                        5
+
+//画面系统的背景图片
+#define  _IMG_系统                                                               6
+
+#define  _BTN_系统_BUTTON1                                                       1
+
+#define  _BTN_系统_BUTTON2                                                       2
+
+#define  _BTN_系统_BUTTON3                                                       3
+
+#define  _BTN_系统_BUTTON4                                                       4
+
+#define  _BTN_系统_BUTTON5                                                       5
+
+#define  _BTN_系统_BUTTON6                                                       6
+
+#define  _BTN_系统_BUTTON7                                                       7
+
+#define  _BTN_系统_BUTTON8                                                       8
+
+//画面生产中动画控件Icon1使用的图片
+#define  _IMG_生产_ICON1                                                         7
+
+#define  _ANIMATION_生产_ICON1                                                   1
+
+#define  _BTN_生产_BUTTON19                                                     20
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY1                                            2
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY2                                            3
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY3                                            4
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY4                                            5
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY5                                            6
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY6                                            7
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY7                                            8
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY8                                            9
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY9                                           10
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY10                                          11
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY11                                          12
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY12                                          13
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY13                                          14
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY14                                          15
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY15                                          16
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY16                                          17
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY17                                          18
+
+#define  _TXT_DIS__生产_TEXT_DISPLAY18                                          19
+
+#define  _BTN_本产品_BUTTON1                                                      1
+
+#define  _BTN_本产品_BUTTON2                                                      2
+
+#define  _BTN_本产品_BUTTON3                                                      3
+
+#define  _BTN_本产品_BUTTON4                                                      4
+
+#define  _BTN_本产品_BUTTON5                                                      5
+
+#define  _BTN_本产品_BUTTON6                                                      6
+
+#define  _BTN_本产品_BUTTON7                                                      7
+
+#define  _BTN_本产品_BUTTON8                                                      8
+
+//画面本产品中动画控件Icon1使用的图片
+#define  _IMG_本产品_ICON1                                                        8
+
+#define  _ANIMATION_本产品_ICON1                                                  9
+
+#define  _BTN_本产品_BUTTON9                                                     10
+
+#define  _BTN_生产厂家_BUTTON1                                                     1
+
+#define  _BTN_生产厂家_BUTTON2                                                     2
+
+#define  _BTN_生产厂家_BUTTON3                                                     3
+
+#define  _BTN_生产厂家_BUTTON4                                                     4
+
+#define  _BTN_生产厂家_BUTTON5                                                     5
+
+#define  _BTN_生产厂家_BUTTON6                                                     6
+
+#define  _BTN_生产厂家_BUTTON7                                                     7
+
+#define  _BTN_生产厂家_BUTTON8                                                     8
+
+//画面生产厂家中动画控件Icon1使用的图片
+#define  _IMG_生产厂家_ICON1                                                       9
+
+#define  _ANIMATION_生产厂家_ICON1                                                 9
+
+#define  _BTN_生产厂家_BUTTON9                                                    10
+
+#define  _BTN_功能与作用_BUTTON1                                                    1
+
+#define  _BTN_功能与作用_BUTTON2                                                    2
+
+#define  _BTN_功能与作用_BUTTON3                                                    3
+
+#define  _BTN_功能与作用_BUTTON4                                                    4
+
+#define  _BTN_功能与作用_BUTTON5                                                    5
+
+#define  _BTN_功能与作用_BUTTON6                                                    6
+
+#define  _BTN_功能与作用_BUTTON7                                                    7
+
+#define  _BTN_功能与作用_BUTTON8                                                    8
+
+//画面功能与作用中动画控件Icon1使用的图片
+#define  _IMG_功能与作用_ICON1                                                     10
+
+#define  _ANIMATION_功能与作用_ICON1                                                9
+
+#define  _BTN_功能与作用_BUTTON9                                                   10
+
+#define  _BTN_测试参数_BUTTON8                                                     8
+
+#define  _BTN_测试参数_BUTTON1                                                     1
+
+#define  _BTN_测试参数_BUTTON2                                                     2
+
+#define  _BTN_测试参数_BUTTON3                                                     3
+
+#define  _BTN_测试参数_BUTTON4                                                     4
+
+#define  _BTN_测试参数_BUTTON5                                                     5
+
+#define  _BTN_测试参数_BUTTON6                                                     6
+
+#define  _BTN_测试参数_BUTTON7                                                     7
+
+#define  _BTN_测试参数_BUTTON9                                                     9
+
+//画面测试参数中动画控件Icon1使用的图片
+#define  _IMG_测试参数_ICON1                                                      11
+
+#define  _ANIMATION_测试参数_ICON1                                                10
+
+#define  _BTN_测试参数_BUTTON10                                                   11
+
+#define  _BTN_测试参数_BUTTON11                                                   12
+
+#define  _BTN_仪器操作_BUTTON1                                                     1
+
+#define  _BTN_仪器操作_BUTTON2                                                     2
+
+#define  _BTN_仪器操作_BUTTON3                                                     3
+
+#define  _BTN_仪器操作_BUTTON4                                                     4
+
+#define  _BTN_仪器操作_BUTTON5                                                     5
+
+#define  _BTN_仪器操作_BUTTON6                                                     6
+
+#define  _BTN_仪器操作_BUTTON7                                                     7
+
+#define  _BTN_仪器操作_BUTTON9                                                     9
+
+//画面仪器操作中动画控件Icon1使用的图片
+#define  _IMG_仪器操作_ICON1                                                      12
+
+#define  _ANIMATION_仪器操作_ICON1                                                 8
+
+#define  _BTN_仪器操作_BUTTON8                                                    10
+
+#define  _BTN_应用领域_BUTTON1                                                     1
+
+#define  _BTN_应用领域_BUTTON2                                                     2
+
+#define  _BTN_应用领域_BUTTON3                                                     3
+
+#define  _BTN_应用领域_BUTTON4                                                     4
+
+#define  _BTN_应用领域_BUTTON5                                                     5
+
+#define  _BTN_应用领域_BUTTON6                                                     6
+
+#define  _BTN_应用领域_BUTTON7                                                     7
+
+#define  _BTN_应用领域_BUTTON9                                                     9
+
+//画面应用领域中动画控件Icon1使用的图片
+#define  _IMG_应用领域_ICON1                                                      13
+
+#define  _ANIMATION_应用领域_ICON1                                                 8
+
+#define  _BTN_应用领域_BUTTON8                                                    10
+
+#define  _BTN_应用案例_BUTTON1                                                     1
+
+#define  _BTN_应用案例_BUTTON2                                                     2
+
+#define  _BTN_应用案例_BUTTON3                                                     3
+
+#define  _BTN_应用案例_BUTTON4                                                     4
+
+#define  _BTN_应用案例_BUTTON5                                                     5
+
+#define  _BTN_应用案例_BUTTON6                                                     6
+
+#define  _BTN_应用案例_BUTTON7                                                     7
+
+#define  _BTN_应用案例_BUTTON9                                                     9
+
+//画面应用案例中动画控件Icon1使用的图片
+#define  _IMG_应用案例_ICON1                                                      14
+
+#define  _ANIMATION_应用案例_ICON1                                                 8
+
+#define  _BTN_应用案例_BUTTON8                                                    10
+
+#define  _BTN_测试参数2_BUTTON1                                                    1
+
+#define  _BTN_测试参数2_BUTTON2                                                    2
+
+#define  _BTN_测试参数2_BUTTON3                                                    3
+
+#define  _BTN_测试参数2_BUTTON4                                                    4
+
+#define  _BTN_测试参数2_BUTTON5                                                    5
+
+#define  _BTN_测试参数2_BUTTON6                                                    6
+
+#define  _BTN_测试参数2_BUTTON7                                                    7
+
+#define  _BTN_测试参数2_BUTTON9                                                    9
+
+//画面测试参数2中动画控件Icon1使用的图片
+#define  _IMG_测试参数2_ICON1                                                     15
+
+#define  _ANIMATION_测试参数2_ICON1                                                8
+
+#define  _BTN_测试参数2_BUTTON8                                                   10
+
+#define  _BTN_测试参数2_BUTTON10                                                  11
+
+//画面显示中动画控件Icon1使用的图片
+#define  _IMG_显示_ICON1                                                        16
+
+#define  _ANIMATION_显示_ICON1                                                   1
+
+#define  _BTN_显示_BUTTON1                                                       2
+
+#define  _BTN_显示_BUTTON2                                                       3
+
+#define  _BTN_显示_BUTTON3                                                       4
+
+#define  _BTN_显示_BUTTON4                                                       5
+
+#define  _BTN_显示_BUTTON5                                                       6
+
+#define  _BTN_显示_BUTTON6                                                       7
+
